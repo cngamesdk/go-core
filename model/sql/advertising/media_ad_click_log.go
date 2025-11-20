44a04ee0d72c045c5a3b2b41664f3828bbb25c20
tangping <442353346@qq.com>
2025-11-20 11:40:18 +0800

    A 增加媒体点击表

--- model/sql/advertising/media_ad_click_log.go
+++ model/sql/advertising/media_ad_click_log.go
@@ -19,23 +19,23 @@ type OdsMediaAdClickLogModel struct {
 	Ad2Id         int64           `json:"ad2_id" gorm:"column:ad2_id;default:0;comment:广告二级ID"`
 	Ad3Id         int64           `json:"ad3_id" gorm:"column:ad3_id;default:0;comment:广告三级ID"`
 	ClickTime     time.Time       `json:"click_time" gorm:"type:datetime(0);column:click_time;comment:点击时间"`
-	ClickId       string          `json:"click_id" gorm:"size:100;column:click_id;comment:点击ID"`
-	RequestId     string          `json:"request_id" gorm:"size:100;column:request_id;comment:点击请求ID"`
-	Csite         string          `json:"csite" gorm:"size:50;column:csite;comment:广告资源位"`
-	Ipv4          string          `json:"ipv4" gorm:"size:20;column:ipv4;comment:ipv4地址"`
-	Ipv6          string          `json:"ipv6" gorm:"size:50;column:ipv6;comment:ipv6地址"`
-	Muid          string          `json:"muid" gorm:"size:50;column:muid;comment:设备号MD5值"`
-	Oaid          string          `json:"oaid" gorm:"size:100;column:oaid;comment:oaid"`
-	Moaid         string          `json:"moaid" gorm:"size:50;column:moaid;comment:oaid MD5值"`
-	DeviceModel   string          `json:"device_model" gorm:"size:50;column:device_model;comment:设备机型"`
-	UserAgent     string          `json:"user_agent" gorm:"size:512;column:user_agent;comment:用户UA"`
-	Callback      string          `json:"callback" gorm:"size:512;column:callback;comment:回调地址"`
-	CallbackParam string          `json:"callback_param" gorm:"size:512;column:callback_param;comment:回调参数"`
-	CaidData      string          `json:"caid_data" gorm:"type:text;column:callback;comment:caid数据"`
-	Mcaid1        string          `json:"mcaid1" gorm:"size:50;column:mcaid1;comment:caid1 MD5值"`
-	Mcaid2        string          `json:"mcaid2" gorm:"size:50;column:mcaid2;comment:caid2 MD5值"`
-	LiveAccount   string          `json:"live_account" gorm:"size:50;column:live_account;comment:直播账号"`
-	OpenId        string          `json:"open_id" gorm:"size:50;column:open_id;comment:open_id"`
+	ClickId       string          `json:"click_id" gorm:"size:100;column:click_id;default:'';comment:点击ID"`
+	RequestId     string          `json:"request_id" gorm:"size:100;column:request_id;default:'';comment:点击请求ID"`
+	Csite         string          `json:"csite" gorm:"size:50;column:csite;default:'';comment:广告资源位"`
+	Ipv4          string          `json:"ipv4" gorm:"size:20;column:ipv4;default:'';comment:ipv4地址"`
+	Ipv6          string          `json:"ipv6" gorm:"size:50;column:ipv6;default:'';comment:ipv6地址"`
+	Muid          string          `json:"muid" gorm:"size:50;column:muid;default:'';comment:设备号MD5值"`
+	Oaid          string          `json:"oaid" gorm:"size:100;column:oaid;default:'';comment:oaid"`
+	Moaid         string          `json:"moaid" gorm:"size:50;column:moaid;default:'';comment:oaid MD5值"`
+	DeviceModel   string          `json:"device_model" gorm:"size:50;column:device_model;default:'';comment:设备机型"`
+	UserAgent     string          `json:"user_agent" gorm:"size:512;column:user_agent;default:'';comment:用户UA"`
+	Callback      string          `json:"callback" gorm:"size:512;column:callback;default:'';comment:回调地址"`
+	CallbackParam string          `json:"callback_param" gorm:"size:512;column:callback_param;default:'';comment:回调参数"`
+	CaidData      string          `json:"caid_data" gorm:"type:text;column:caid_data;comment:caid数据"`
+	Mcaid1        string          `json:"mcaid1" gorm:"size:50;column:mcaid1;default:'';comment:caid1 MD5值"`
+	Mcaid2        string          `json:"mcaid2" gorm:"size:50;column:mcaid2;default:'';comment:caid2 MD5值"`
+	LiveAccount   string          `json:"live_account" gorm:"size:50;column:live_account;default:'';comment:直播账号"`
+	OpenId        string          `json:"open_id" gorm:"size:50;column:open_id;default:'';comment:open_id"`
 	Db            func() *gorm.DB `json:"-" gorm:"-"`
 }
 
