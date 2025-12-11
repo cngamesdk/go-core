39b27d1a37f69bdbb1b547d14b5c417bf8848d05
tangping <442353346@qq.com>
2025-12-11 18:27:09 +0800

    M 优化报表字段

--- model/sql/report/day_root_game_back_login_active_log.go
+++ model/sql/report/day_root_game_back_login_active_log.go
@@ -11,7 +11,7 @@ type DwsDayRootGameBackLoginActiveLogModel struct {
 	sql.SqlBaseModel
 	PlatformId  int64           `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_unique"`
 	LoginDate   string          `json:"login_date" gorm:"type:date;column:login_date;comment:登录日期;uniqueIndex:ix_unique"`
-	RootGameId  int64           `json:"root_game_id" gorm:"column:root_game_id;default:0;comment:根游戏ID;uniqueIndex:ix_unique"`
+	GameId      int64           `json:"game_id" gorm:"column:game_id;default:0;comment:子游戏ID;uniqueIndex:ix_unique"`
 	AgentId     int64           `json:"agent_id" gorm:"column:agent_id;default:0;comment:渠道ID;uniqueIndex:ix_unique"`
 	SiteId      int64           `json:"site_id" gorm:"column:site_id;default:0;comment:广告位ID;uniqueIndex:ix_unique"`
 	Ad3Id       int64           `json:"ad3_id" gorm:"column:ad3_id;default:0;comment:广告三级ID;uniqueIndex:ix_unique"`
--- model/sql/report/day_root_game_back_pay_active_log.go
+++ model/sql/report/day_root_game_back_pay_active_log.go
@@ -9,18 +9,19 @@ import (
 // DwsDayRootGameBackPayActiveLogModel 根游戏回流每日活跃报表
 type DwsDayRootGameBackPayActiveLogModel struct {
 	sql.SqlBaseModel
-	PlatformId  int64           `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_unique"`
-	PayDate     string          `json:"pay_date" gorm:"type:date;column:pay_date;comment:付费日期;uniqueIndex:ix_unique"`
-	RootGameId  int64           `json:"root_game_id" gorm:"column:root_game_id;default:0;comment:根游戏ID;uniqueIndex:ix_unique"`
-	AgentId     int64           `json:"agent_id" gorm:"column:agent_id;default:0;comment:渠道ID;uniqueIndex:ix_unique"`
-	SiteId      int64           `json:"site_id" gorm:"column:site_id;default:0;comment:广告位ID;uniqueIndex:ix_unique"`
-	Ad3Id       int64           `json:"ad3_id" gorm:"column:ad3_id;default:0;comment:广告三级ID;uniqueIndex:ix_unique"`
-	RegDate     string          `json:"reg_date" gorm:"type:date;column:reg_date;comment:注册日期;uniqueIndex:ix_unique"`
-	Pay         int             `json:"pay" gorm:"column:pay;comment:付费数;"`
-	ActiveDays  int             `json:"active_days" gorm:"column:active_days;default:0;comment:注册到付费的活跃天数;"`
-	ActiveCount int             `json:"active_count" gorm:"column:active_count;default:0;comment:注册到付费的活跃人数;"`
-	PayMoneySum int             `json:"pay_money_sum" gorm:"column:pay_money_sum;default:0;comment:付费金额;"`
-	Db          func() *gorm.DB `json:"-" gorm:"-"`
+	PlatformId   int64           `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_unique"`
+	PayDate      string          `json:"pay_date" gorm:"type:date;column:pay_date;comment:付费日期;uniqueIndex:ix_unique"`
+	FirstPayDate string          `json:"first_pay_date" gorm:"type:date;column:first_pay_date;comment:首次付费日期"`
+	GameId       int64           `json:"game_id" gorm:"column:game_id;default:0;comment:子游戏ID;uniqueIndex:ix_unique"`
+	AgentId      int64           `json:"agent_id" gorm:"column:agent_id;default:0;comment:渠道ID;uniqueIndex:ix_unique"`
+	SiteId       int64           `json:"site_id" gorm:"column:site_id;default:0;comment:广告位ID;uniqueIndex:ix_unique"`
+	Ad3Id        int64           `json:"ad3_id" gorm:"column:ad3_id;default:0;comment:广告三级ID;uniqueIndex:ix_unique"`
+	RegDate      string          `json:"reg_date" gorm:"type:date;column:reg_date;comment:注册日期;uniqueIndex:ix_unique"`
+	Pay          int             `json:"pay" gorm:"column:pay;comment:付费数;"`
+	ActiveDays   int             `json:"active_days" gorm:"column:active_days;default:0;comment:注册到付费的活跃天数;"`
+	ActiveCount  int             `json:"active_count" gorm:"column:active_count;default:0;comment:注册到付费的活跃人数;"`
+	PayMoneySum  int             `json:"pay_money_sum" gorm:"column:pay_money_sum;default:0;comment:付费金额;"`
+	Db           func() *gorm.DB `json:"-" gorm:"-"`
 }
 
 func (receiver *DwsDayRootGameBackPayActiveLogModel) TableName() string {
