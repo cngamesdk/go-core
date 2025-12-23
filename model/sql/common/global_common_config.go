be6f020b508c952820b3d26c29d0a3b916cc5e37
tangping <442353346@qq.com>
2025-12-23 17:54:26 +0800

    A 全局通用配置模型新增处理函数

--- model/sql/common/global_common_config.go
+++ model/sql/common/global_common_config.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"context"
 	sql2 "github.com/cngamesdk/go-core/model/sql"
 	"gorm.io/gorm"
 )
@@ -10,4 +11,23 @@ type GlobalCommonConfigModel struct {
 	Db                    func() *gorm.DB `json:"-" gorm:"-"`
 	PlatformId            int             `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID"`
 	GamePackagingToolPath string          `json:"game_packaging_tool_path" gorm:"column:game_packaging_tool_path;default:'';comment:游戏打包工具路径"`
+}
+
+func (receiver *GlobalCommonConfigModel) TableName() string {
+	return "dim_global_common_config"
+}
+
+func (receiver *GlobalCommonConfigModel) Take(ctx context.Context, fields string, query string, args ...interface{}) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Select(fields).Where(query, args...).Take(receiver).Error
+	return
+}
+
+func (receiver *GlobalCommonConfigModel) Create(ctx context.Context) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Create(receiver).Error
+	return
+}
+
+func (receiver *GlobalCommonConfigModel) Updates(ctx context.Context, query interface{}, args ...interface{}) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Where(query, args).Updates(receiver).Error
+	return
 }
