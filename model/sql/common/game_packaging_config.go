3c1f5fd84290d168141ccfc8d5e58eaf774f3f16
tangping <442353346@qq.com>
2025-12-24 15:44:56 +0800

    M 优化打包相关模型字段

--- model/sql/common/game_packaging_config.go
+++ model/sql/common/game_packaging_config.go
@@ -9,9 +9,9 @@ import (
 type GamePackagingConfigModel struct {
 	sql2.SqlBaseModel
 	Db              func() *gorm.DB `json:"-" gorm:"-"`
-	PlatformId      int             `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID"`
-	GameId          int             `json:"game_id" gorm:"column:game_id;default:0;comment:游戏ID"`
-	CommonMedia     string          `json:"common_media" gorm:"column:common_media;default:'';comment:常见媒体"`
+	PlatformId      int             `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;index:ix_plat_game_media"`
+	GameId          int             `json:"game_id" gorm:"column:game_id;default:0;comment:游戏ID;index:ix_plat_game_media"`
+	CommonMedia     string          `json:"common_media" gorm:"size:50;column:common_media;default:'';comment:常见媒体;index:ix_plat_game_media"`
 	GamePackagePath string          `json:"game_package_path" gorm:"size:512;column:game_package_path;default:'';comment:游戏包路径"`
 	GamePackageHash string          `json:"game_package_hash" gorm:"size:100;column:game_package_hash;default:'';comment:游戏包hash摘要"`
 	Status          string          `json:"status" gorm:"size:50;column:status;default:'';comment:状态"`
