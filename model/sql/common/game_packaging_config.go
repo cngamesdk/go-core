1a5e42cb8f87953cdbee3ee0125d5d95f260ced1
tangping <442353346@qq.com>
2026-02-10 18:08:57 +0800

    M 优化媒体打包配置表

--- model/sql/common/game_packaging_config.go
+++ model/sql/common/game_packaging_config.go
@@ -16,7 +16,7 @@ type DimGamePackagingConfigModel struct {
 	Db              func() *gorm.DB `json:"-" gorm:"-"`
 	PlatformId      int             `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;index:ix_plat_game_media"`
 	GameId          int             `json:"game_id" gorm:"column:game_id;default:0;comment:游戏ID;index:ix_plat_game_media"`
-	CommonMedia     string          `json:"common_media" gorm:"size:50;column:common_media;default:'';comment:常见媒体;index:ix_plat_game_media"`
+	MediaCode       string          `json:"media_code" gorm:"size:50;column:media_code;default:'';comment:媒体码;index:ix_plat_game_media"`
 	GamePackagePath string          `json:"game_package_path" gorm:"size:512;column:game_package_path;default:'';comment:游戏包路径"`
 	GamePackageHash string          `json:"game_package_hash" gorm:"size:100;column:game_package_hash;default:'';comment:游戏包hash摘要"`
 	Status          string          `json:"status" gorm:"size:50;column:status;default:'';comment:状态"`
