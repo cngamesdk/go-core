f1c43fa22fa17c98b9c3a0f158d7df58e394cceb
tangping <442353346@qq.com>
2026-02-20 09:55:34 +0800

    A 广告授权帐户增加字段-果儿

--- model/sql/advertising/advertising_media_account.go
+++ model/sql/advertising/advertising_media_account.go
@@ -27,14 +27,18 @@ func GetAdvertisingMediaAccountStatusName(req string) string {
 // DimAdvertisingMediaAccountModel 广告媒体维度表
 type DimAdvertisingMediaAccountModel struct {
 	sql2.SqlBaseModel
-	PlatformId  int64           `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_plat_account"`
-	AccountName string          `json:"account_name" gorm:"size:100;column:account_name;default:'';comment:帐户名称"`
-	AccountId   uint64          `json:"account_id" gorm:"size:100;column:account_id;default:0;comment:帐户ID;uniqueIndex:ix_plat_account"`
-	Status      string          `json:"status" gorm:"size:100;column:status;default:'';comment:状态"`
-	ManageId    int64           `json:"manage_id" gorm:"column:manage_id;default:0;comment:超管主键ID"`
-	Role        string          `json:"role" gorm:"column:role;default:'';comment:角色"`
-	Extension   string          `json:"extension" gorm:"type:json;column:extension;comment:扩展字段"`
-	Db          func() *gorm.DB `json:"-" gorm:"-"`
+	PlatformId            int64                 `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_plat_account"`
+	AccountName           string                `json:"account_name" gorm:"size:100;column:account_name;default:'';comment:帐户名称"`
+	AccountId             uint64                `json:"account_id" gorm:"size:100;column:account_id;default:0;comment:帐户ID;uniqueIndex:ix_plat_account"`
+	Status                string                `json:"status" gorm:"size:100;column:status;default:'';comment:状态"`
+	ManageId              int64                 `json:"manage_id" gorm:"column:manage_id;default:0;comment:超管主键ID"`
+	Role                  string                `json:"role" gorm:"column:role;default:'';comment:角色"`
+	Extension             string                `json:"extension" gorm:"type:json;column:extension;comment:扩展字段"`
+	AccessToken           string                `json:"access_token" gorm:"size:100;column:access_token;default:'';comment:授权令牌"`
+	RefreshToken          string                `json:"refresh_token" gorm:"size:512;column:refresh_token;default:'';comment:刷新令牌"`
+	ExpiresAt             sql2.MyCustomDatetime `json:"expires_at" gorm:"type:datetime(0);column:expires_at;comment:Access_Token过期时间"`
+	RefreshTokenExpiresAt sql2.MyCustomDatetime `json:"refresh_token_expires_at" gorm:"type:datetime(0);column:refresh_token_expires_at;comment:refresh_token过期时间"`
+	Db                    func() *gorm.DB       `json:"-" gorm:"-"`
 }
 
 func (receiver *DimAdvertisingMediaAccountModel) TableName() string {
