c9417df5a382d8a72619c07dcf0478192fd4ca54
tangping <442353346@qq.com>
2026-02-25 16:50:20 +0800

    M 优化授权&媒体帐户表

---
+++ model/sql/advertising/advertising_media_auth.go
@@ -0,0 +1,61 @@
+package advertising
+
+import (
+	"context"
+	sql2 "github.com/cngamesdk/go-core/model/sql"
+	"gorm.io/gorm"
+)
+
+const (
+	DimAdvertisingMediaAuthStatusNormal = sql2.StatusNormal
+	DimAdvertisingMediaAuthStatusDelete = sql2.StatusDelete
+)
+
+var AdvertisingMediaAuthStatuss = map[string]string{
+	DimAdvertisingMediaAuthStatusNormal: "正常",
+	DimAdvertisingMediaAuthStatusDelete: "删除",
+}
+
+func GetAdvertisingMediaAuthStatusName(req string) string {
+	resp, ok := AdvertisingMediaAuthStatuss[req]
+	if !ok {
+		return ""
+	}
+	return resp
+}
+
+// DimAdvertisingMediaAuthModel 广告媒体超管维度表
+type DimAdvertisingMediaAuthModel struct {
+	sql2.SqlBaseModel
+	PlatformId            int64                 `json:"platform_id" gorm:"column:platform_id;default:0;comment:平台ID;uniqueIndex:ix_plat_account"`
+	AccountName           string                `json:"account_name" gorm:"size:100;column:account_name;default:'';comment:帐户名称"`
+	AccountId             uint64                `json:"account_id" gorm:"size:100;column:account_id;default:0;comment:帐户ID;uniqueIndex:ix_plat_account"`
+	Status                string                `json:"status" gorm:"size:100;column:status;default:'';comment:状态"`
+	Extension             string                `json:"extension" gorm:"type:json;column:extension;comment:扩展字段"`
+	AccessToken           string                `json:"access_token" gorm:"size:100;column:access_token;default:'';comment:授权令牌"`
+	RefreshToken          string                `json:"refresh_token" gorm:"size:512;column:refresh_token;default:'';comment:刷新令牌"`
+	ExpiresAt             sql2.MyCustomDatetime `json:"expires_at" gorm:"type:datetime(0);column:expires_at;comment:Access_Token过期时间"`
+	RefreshTokenExpiresAt sql2.MyCustomDatetime `json:"refresh_token_expires_at" gorm:"type:datetime(0);column:refresh_token_expires_at;comment:refresh_token过期时间"`
+	DeveloperId           int64                 `json:"developer_id" gorm:"column:developer_id;default:0;comment:开发者主键ID"`
+	Code                  string                `json:"code" gorm:"size:50;column:code;default:'';comment:媒体码;uniqueIndex:ix_code_account"`
+	Db                    func() *gorm.DB       `json:"-" gorm:"-"`
+}
+
+func (receiver *DimAdvertisingMediaAuthModel) TableName() string {
+	return "dim_advertising_media_auth"
+}
+
+func (receiver *DimAdvertisingMediaAuthModel) Take(ctx context.Context, fields string, query string, args ...interface{}) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Select(fields).Where(query, args...).Take(receiver).Error
+	return
+}
+
+func (receiver *DimAdvertisingMediaAuthModel) Create(ctx context.Context) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Create(receiver).Error
+	return
+}
+
+func (receiver *DimAdvertisingMediaAuthModel) Updates(ctx context.Context, query interface{}, args ...interface{}) (err error) {
+	err = receiver.Db().WithContext(ctx).Table(receiver.TableName()).Where(query, args...).Updates(receiver).Error
+	return
+}
